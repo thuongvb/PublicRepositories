00be6722c337dd42d3d2f735e4a54e17d3e3fb96
Bùi Văn Thương <thuongvanbui39@gmail.com>
2024-09-20 20:48:49 +0700

    Init project.

---
+++ PublicRepositories/PublicRepositories/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PublicRepositories
+//
+//  Created by Bùi Văn Thương on 9/20/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PublicRepositories/PublicRepositories/PublicRepositoriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PublicRepositoriesApp.swift
+//  PublicRepositories
+//
+//  Created by Bùi Văn Thương on 9/20/24.
+//
+
+import SwiftUI
+
+@main
+struct PublicRepositoriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
